c19f8e71b50fffb9361c72f9523a5f934a6a9019
tomato potato <nimaigos108@gmail.com>
2026-09-10 20:44:31 -0700

    feat(stats): add rank stat

--- src/consts.ts
+++ src/consts.ts
@@ -3,3 +3,32 @@
 
 export const SITE_TITLE = 'Mouldy Shoe';
 export const SITE_DESCRIPTION = '';
+
+// hardcoding rank tiers bc there's no public api for rl ranks :(
+export const RANK_TIERS = [
+	'B1',
+	'B2',
+	'B3',
+	'S1',
+	'S2',
+	'S3',
+	'G1',
+	'G2',
+	'G3',
+	'P1',
+	'P2',
+	'P3',
+	'D1',
+	'D2',
+	'D3',
+	'C1',
+	'C2',
+	'C3',
+	'GC1',
+	'GC2',
+	'GC3',
+	'SSL',
+] as const;
+
+/** Must be a value from RANK_TIERS. */
+export const CURRENT_RANK: (typeof RANK_TIERS)[number] = 'GC2';
